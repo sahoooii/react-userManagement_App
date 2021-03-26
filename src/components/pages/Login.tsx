db5caaffbee0625312ade775860eec7e9523b4a8
saho <75118062+sahoooii@users.noreply.github.com>
2021-03-26 16:59:35 +0900

    Initial commit

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
+
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
+
+export const Login: VFC = memo(() => {
+  const { login, loading } = useAuth();
+
+  const [userId, setuserId] = useState("");
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setuserId(e.target.value);
+
+  const onClickLogin = () => login(userId);
+
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          User管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={8} py={4} px={10}>
+          <Input
+            placeholder="User ID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
+        </Stack>
+      </Box>
+    </Flex>
+  );
+});
